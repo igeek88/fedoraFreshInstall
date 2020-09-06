f403af96225742506d40e6254442b7dd15331840
Iván Weinberg <6950405+igeek88@users.noreply.github.com>
2020-09-06 11:42:28 -0300

    Yet more programs. Added spotify installation via flatpak as well.

--- fedoraFreshInstall.sh
+++ fedoraFreshInstall.sh
@@ -4,7 +4,7 @@ sudo dnf makecache
 sudo dnf -y update
 sudo dnf -y upgrade
 sudo dnf -y install \
-	wget vim terminator curl sudo dnf-plugin-system-upgrade gpg keepass kernel-devel kernel-headers gcc make dkms \
+	wget vim terminator curl sudo dnf-plugin-system-upgrade gpg keepass kernel-devel kernel-headers gcc make dkms flatpak \
 	acpid libglvnd-glx libglvnd-opengl libglvnd-devel pkgconfig vdpauinfo libva-vdpau-driver libva-utils xclip gimp pwgen
 cp .bash_aliases ~/.bash_aliases
 cat addToBashrc >> ~/.bashrc
@@ -14,3 +14,5 @@ cd /home/dl/installers
 wget -vdc https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 sudo dnf -y install ./google-chrome-stable_current_x86_64.rpm
 rm -rvi /home/dl
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak install flathub com.spotify.Client
