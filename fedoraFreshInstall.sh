e073330d1d9a846ce907419418e378894770be01
Iván Weinberg <6950405+igeek88@users.noreply.github.com>
2020-09-06 00:07:16 -0300

    Modified destination of bash files.

--- fedoraFreshInstall.sh
+++ fedoraFreshInstall.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-dnf makecache
-dnf -y update
-dnf -y upgrade
-dnf -y install wget vim terminator curl dnf-plugin-system-upgrade gpg keepass
-cp .bash_aliases /etc/.bash_aliases
-cp .bashrc /etc/.bashrc
-source /etc/.bash_aliases /etc/.bashrc
+sudo dnf makecache
+sudo dnf -y update
+sudo dnf -y upgrade
+sudo dnf -y install wget vim terminator curl sudo dnf-plugin-system-upgrade gpg keepass
+cp .bash_aliases ~/.bash_aliases
+cp .bashrc ~/.bashrc
+source ~/.bash_aliases ~/etc/.bashrc
 mkdir -vp /home/dl/installers
 cd /home/dl/installers
 wget -vdc https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-dnf -y install ./google-chrome-stable_current_x86_64.rpm
+sudo dnf -y install ./google-chrome-stable_current_x86_64.rpm
 rm -rvi /home/dl
