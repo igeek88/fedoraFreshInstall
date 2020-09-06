9fd34918631b72c883d75b7f97eea9df9ef24e82
Iván Weinberg <6950405+igeek88@users.noreply.github.com>
2020-09-06 10:54:10 -0300

    modified bashrc line.

--- fedoraFreshInstall.sh
+++ fedoraFreshInstall.sh
@@ -7,7 +7,7 @@ sudo dnf -y install \
 	wget vim terminator curl sudo dnf-plugin-system-upgrade gpg keepass kernel-devel kernel-headers gcc make dkms \
 	acpid libglvnd-glx libglvnd-opengl libglvnd-devel pkgconfig vdpauinfo libva-vdpau-driver libva-utils xclip gimp pwgen
 cp .bash_aliases ~/.bash_aliases
-cp .bashrc ~/.bashrc
+cat addToBashrc >> ~/.bashrc
 source ~/.bash_aliases ~/etc/.bashrc
 mkdir -vp /home/dl/installers
 cd /home/dl/installers
